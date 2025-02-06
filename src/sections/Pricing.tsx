714601b20ef3342566820af657b28ed1307b3075
saadaltafofficial <saadbeenco@gmail.com>
2025-02-07 00:26:46 +0500

    add unique keys to list items in Pricing component for improved rendering

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -67,8 +67,8 @@ export const Pricing = () => {
         </p>
         </div>
         <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
-          {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }) => (
-            <div className={twMerge("card", inverse === true && "border-black bg-black text-white")}>
+          {pricingTiers.map(({ title, monthlyPrice, buttonText, popular, inverse, features }, index) => (
+            <div key={index} className={twMerge("card", inverse === true && "border-black bg-black text-white")}>
               <div className="flex justify-between">
               <h3 className={twMerge("text-lg font-bold text-black/50", inverse === true && "text-white/60")}>{title}</h3>
               {popular === true && (
@@ -95,8 +95,8 @@ export const Pricing = () => {
               </div>
               <button className={twMerge("btn btn-primary w-full mt-[30px]", inverse===true && "bg-white text-black")}>{buttonText}</button>
               <ul className="flex flex-col gap-5 mt-8">
-                {features.map((feature) => (
-                  <li className="text-sm flex items-center gap-4">
+                {features.map((feature, index) => (
+                  <li key={index} className="text-sm flex items-center gap-4">
                     <CheckIcon  className="h-6 w-6"/>
                     <span>{feature}</span>
                   </li>)) }
